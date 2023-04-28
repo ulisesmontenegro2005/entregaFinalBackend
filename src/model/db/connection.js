5343184a643990673852dcb78ef9ff816dcaea16
Ulises Montenegro <ulinicolasmonte1@gmail.com>
2023-04-27 21:34:36 -0300

    2.0

--- src/model/db/connection.js
+++ src/model/db/connection.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
-    const connect = async () => {
-        let connection = await mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true });
-    };
+const connect = async () => {
+    let connection = await mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true });
+};
 
 export default {
     connect
